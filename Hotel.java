46e8d7c9b2ba6f8d99abab860073e7aa5acf2a50
Anzala63975 <anzala.63975@iqra.edu.pk>
2025-12-19 02:57:12 +0500

    Add Hotel.java class

---
+++ Hotel.java
@@ -0,0 +1,56 @@
+package HotelSystem;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hotel {
+    private String name;
+    private List<Room> rooms;
+    private List<RoomType> roomTypes;
+    private List<Reservation> reservations;
+
+    public Hotel(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Error: Hotel name cannot be empty.");
+        }
+        this.name = name;
+        this.rooms = new ArrayList<>();
+        this.roomTypes = new ArrayList<>();
+        this.reservations = new ArrayList<>();
+    }
+
+    public void addRoomType(RoomType type) {
+        if (type != null) roomTypes.add(type);
+    }
+
+    public void addRoom(Room room) {
+        if (room != null) rooms.add(room);
+    }
+    
+    public List<Room> getRooms() {
+        return rooms;
+    }
+    
+    // Checks if we have enough physical rooms of this type
+    public boolean isTypeAvailable(RoomType type, int quantityNeeded) {
+        int count = 0;
+        for (Room r : rooms) {
+            if (r.getRoomType().equals(type) && r.isFree()) {
+                count++;
+            }
+        }
+        return count >= quantityNeeded;
+    }
+
+    public Reservation createReservation(LocalDate start, LocalDate end, ReservePayer payer) {
+        int newId = reservations.size() + 100;
+        Reservation res = new Reservation(newId, start, end, payer);
+        reservations.add(res);
+        return res;
+    }
+    
+    public String getName() {
+        return name;
+    }
+}
